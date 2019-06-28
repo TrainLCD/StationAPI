88c5bc7de2c2905ef02739cd00d1604faf17854d
TinyKitten <catwalk8205@gmail.com>
2019-06-29 08:07:26 +0900

    使えないデータを削減

--- lib/station_api_web/schema/content_types.ex
+++ lib/station_api_web/schema/content_types.ex
@@ -13,8 +13,6 @@ defmodule StationApiWeb.Schema.ContentTypes do
     field :pref_id, :integer
     field :group_id, :integer
     field :name, :string
-    field :name_r, :string
-    field :name_k, :string
   end
 
   object :line do
