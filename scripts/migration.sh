7225a6f8d2934348c043e866b79f5d1b04d20726
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-05-07 11:48:21 +0900

    Revert "デプロイスクリプトを同じpackageに変更"
    This reverts commit 00ff7eab57ddfbfe7af04b77591ebd2f6ffd4320.

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -2,6 +2,7 @@
 
 mysql -f -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -e "CREATE DATABASE ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;" || true
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < ./migrations/create_table.sql
+npm install
 rm ./dump.sql || true
 node ./sqlgen.js
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < ./tmp.sql
