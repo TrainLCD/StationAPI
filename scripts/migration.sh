5923ea62b45ccd174ebd864a33fbe29b89471c8c
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-12-15 13:29:22 +0900

    bash

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 mysql -f -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -e "CREATE DATABASE ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;" || true
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < ./migrations/create_table.sql
