581133157a3c12d1e9ddddb5fef200b1f8bb2e0f
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-04-29 09:00:07 +0900

    gzip圧縮

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
