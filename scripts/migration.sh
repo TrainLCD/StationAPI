ce25200e4de42b75459d349774a3af69357f9f47
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-04-30 12:25:24 +0900

    Dockerイメージ軽量化

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -3,5 +3,4 @@
 sleep 30
 mysql -f -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < ./migrations/create_table.sql
-node ./scripts/sqlgen.js
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < ./tmp.sql
