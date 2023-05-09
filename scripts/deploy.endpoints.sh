407a2215ed4ce5b90b1473c877b9f2016bbfdddf
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-05-09 15:28:46 +0900

    不要ファイルの削除

--- scripts/deploy.endpoints.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-protoc \
-    --include_imports \
-    --include_source_info \
-    --proto_path=. \
-    --descriptor_set_out=api_descriptor.pb \
-    ./proto/stationapi.proto
-gcloud endpoints services deploy api_descriptor.pb api_config.yaml
