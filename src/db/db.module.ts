c50a14b47b6c5866b81fefa27d1ad920a7a3905b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-12 16:00:16 +0900

    GCPのDB接続時プールを使う

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -4,13 +4,29 @@ const mysql = require('mysql2');
 
 export const DB_CONNECTION = 'DB_CONNECTION';
 
-const dbProvider = {
+const dbProviderGCP = {
   provide: DB_CONNECTION,
-  useValue: mysql.createConnection(process.env.DATABASE_URL),
+  useValue: mysql.createPool({
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    socketPath: `${process.env.MYSQL_SOCKET}/${process.env.INSTANCE_CONNECTION_NAME}`,
+  }),
+};
+
+const dbProviderLocal = {
+  provide: DB_CONNECTION,
+  useValue: mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  }),
 };
 
 @Module({
-  providers: [dbProvider],
-  exports: [dbProvider],
+  providers: [process.env.IS_GCP ? dbProviderGCP : dbProviderLocal],
+  exports: [process.env.IS_GCP ? dbProviderGCP : dbProviderLocal],
 })
 export class DbModule {}
