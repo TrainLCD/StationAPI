7aa579410540e5c9319333bd286dae2105260fed
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-14 12:20:40 +0900

    build.rsを元に戻した

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/stationapi.proto")?;
+    let mut prost_build_config = prost_build::Config::new();
+    prost_build_config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure().compile_with_config(
+        prost_build_config,
+        &["proto/stationapi.proto"],
+        &["proto"],
+    )?;
     Ok(())
 }
