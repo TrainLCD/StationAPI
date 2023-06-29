142788406fe6b5ebdd50b70ab8b89d7449f46b9d
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-29 23:17:01 +0900

    dummy付けるアトリビュート追加

--- build.rs
+++ build.rs
@@ -6,6 +6,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .type_attribute("LineSymbol", "#[derive(fake::Dummy)]")
         .type_attribute("StationNumber", "#[derive(fake::Dummy)]")
         .type_attribute("CompanyResponse", "#[derive(fake::Dummy)]")
+        .type_attribute("LineResponse", "#[derive(fake::Dummy)]")
+        .type_attribute("StationResponse", "#[derive(fake::Dummy)]")
         .compile_with_config(prost_build_config, &["proto/stationapi.proto"], &["proto"])
         .unwrap();
 
