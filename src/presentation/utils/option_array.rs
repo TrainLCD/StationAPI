ab6c882a42a6ef6328bd3efe7b8236b4123a72c3
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-29 22:36:27 +0900

    delete_option_from_string_vecテスト追加

--- src/presentation/utils/option_array.rs
+++ src/presentation/utils/option_array.rs
@@ -8,3 +8,15 @@ pub fn delete_option_from_string_vec(vec: Vec<Option<String>>) -> Vec<String> {
         })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn delete_option_from_string_vec() {
+        use super::*;
+
+        let vec = vec![Some("a".to_string()), None, Some("b".to_string())];
+        let vec = delete_option_from_string_vec(vec);
+        assert_eq!(vec, vec!["a".to_string(), "b".to_string()]);
+    }
+}
