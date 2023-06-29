69581bdb0094ec7a4eef3378d59cfea688aee014
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-29 22:20:15 +0900

    infrastructureのユニットテスト追加

--- src/infrastructure/error.rs
+++ src/infrastructure/error.rs
@@ -5,3 +5,16 @@ impl From<sqlx::Error> for DomainError {
         DomainError::InfrastructureError(anyhow::Error::new(error))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn from() {
+        use super::DomainError;
+
+        let error = sqlx::Error::RowNotFound;
+        let domain_error = DomainError::from(error);
+
+        assert!(matches!(domain_error, DomainError::InfrastructureError(_)));
+    }
+}
