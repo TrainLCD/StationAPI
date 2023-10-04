d95d8dfeb062c9b0211675a3971e9276bcd107e8
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-10-05 08:54:28 +0900

    なんかget_by_coordinatesから種別自動選択消えてた

--- src/infrastructure/station_repository.rs
+++ src/infrastructure/station_repository.rs
@@ -495,50 +495,62 @@ impl InternalStationRepository {
         conn: &mut MySqlConnection,
     ) -> Result<Vec<Station>, DomainError> {
         let rows = sqlx::query_as::<_, StationRow>(
-            "SELECT l.*,
-            s.*,
-            COALESCE(a.line_name, l.line_name) AS line_name,
-            COALESCE(a.line_name_k, l.line_name_k) AS line_name_k,
-            COALESCE(a.line_name_h, l.line_name_h) AS line_name_h,
-            COALESCE(a.line_name_r, l.line_name_r) AS line_name_r,
-            COALESCE(a.line_name_zh, l.line_name_zh) AS line_name_zh,
-            COALESCE(a.line_name_ko, l.line_name_ko) AS line_name_ko,
-            COALESCE(a.line_color_c, l.line_color_c) AS line_color_c,
-            (
-              6371 * acos(
-                cos(
-                  radians(s.lat)
-                ) * cos(
-                  radians(?)
-                ) * cos(
-                  radians(?) - radians(s.lon)
-                ) + sin(
-                  radians(s.lat)
-                ) * sin(
-                  radians(?)
-                )
-              )
-            ) AS distance,
-            (
-              SELECT
-                COUNT(sst.line_group_cd) 
-              FROM
-                station_station_types AS sst
-              WHERE
-                s.station_cd = sst.station_cd
-                AND sst.pass <> 1
-            ) AS station_types_count
-          FROM
-            (`stations` AS s, `lines` AS l)
-            LEFT OUTER JOIN `line_aliases` AS la ON la.station_cd = s.station_cd
-            LEFT OUTER JOIN `aliases` AS a ON a.id = la.alias_cd
-          WHERE
-            s.line_cd = l.line_cd
-            AND s.e_status = 0
-          ORDER BY
-            distance
-          LIMIT
-            ?",
+            "SELECT 
+                  l.*, 
+                  s.*, 
+                  COALESCE(a.line_name, l.line_name) AS line_name, 
+                  COALESCE(a.line_name_k, l.line_name_k) AS line_name_k, 
+                  COALESCE(a.line_name_h, l.line_name_h) AS line_name_h, 
+                  COALESCE(a.line_name_r, l.line_name_r) AS line_name_r, 
+                  COALESCE(a.line_name_zh, l.line_name_zh) AS line_name_zh, 
+                  COALESCE(a.line_name_ko, l.line_name_ko) AS line_name_ko, 
+                  COALESCE(a.line_color_c, l.line_color_c) AS line_color_c, 
+                  (
+                    6371 * acos(
+                      cos(
+                        radians(s.lat)
+                      ) * cos(
+                        radians(?)
+                      ) * cos(
+                        radians(?) - radians(s.lon)
+                      ) + sin(
+                        radians(s.lat)
+                      ) * sin(
+                        radians(?)
+                      )
+                    )
+                  ) AS distance, 
+                  (
+                    SELECT 
+                      COUNT(sst.line_group_cd) 
+                    FROM 
+                      station_station_types AS sst 
+                    WHERE 
+                      s.station_cd = sst.station_cd 
+                      AND sst.pass <> 1
+                  ) AS station_types_count 
+                FROM 
+                  (`stations` AS s, `lines` AS l) 
+                  LEFT OUTER JOIN `line_aliases` AS la ON la.station_cd = s.station_cd 
+                  LEFT OUTER JOIN `aliases` AS a ON a.id = la.alias_cd 
+                  LEFT OUTER JOIN `station_station_types` AS sst ON sst.line_group_cd = (
+                    SELECT 
+                      sst.line_group_cd 
+                    FROM 
+                      `station_station_types` AS sst 
+                    WHERE 
+                      sst.type_cd IN (100, 101, 300, 301) 
+                      AND sst.station_cd = s.station_cd 
+                    LIMIT 
+                      1
+                  ) LEFT OUTER JOIN `types` AS t ON t.type_cd = sst.type_cd 
+                WHERE 
+                  s.line_cd = l.line_cd 
+                  AND s.e_status = 0 
+                ORDER BY 
+                  distance 
+                LIMIT 
+                  ?",
         )
         .bind(latitude)
         .bind(longitude)
