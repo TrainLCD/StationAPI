7c797765db82e21abb0a76e919b2f983dc106fd7
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-10-31 21:56:15 +0900

    駅名検索を部分一致に戻す

--- src/infrastructure/station_repository.rs
+++ src/infrastructure/station_repository.rs
@@ -570,7 +570,7 @@ impl InternalStationRepository {
         limit: Option<u32>,
         conn: &mut MySqlConnection,
     ) -> Result<Vec<Station>, DomainError> {
-        let station_name = format!("{}%", station_name);
+        let station_name = format!("%{}%", station_name);
 
         let rows = sqlx::query_as::<_, StationRow>(
             "SELECT
