429a65600b2fe6bf47052b6938855ad70b9b0ce3
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-04-17 01:31:08 +0900

    ナンバリング情報がバグってた

--- stationapi/src/use_case/interactor/query.rs
+++ stationapi/src/use_case/interactor/query.rs
@@ -512,15 +512,15 @@ where
                     station_number: station_number_string,
                 };
                 station_numbers.push(station_number);
-            };
-
-            let station_number = StationNumber {
-                line_symbol: "".to_string(),
-                line_symbol_color: sym_color,
-                line_symbol_shape: sym_shape,
-                station_number: station_number.to_string(),
-            };
-            station_numbers.push(station_number);
+            } else {
+                let station_number = StationNumber {
+                    line_symbol: "".to_string(),
+                    line_symbol_color: sym_color,
+                    line_symbol_shape: sym_shape,
+                    station_number: station_number.to_string(),
+                };
+                station_numbers.push(station_number);
+            }
         }
 
         station_numbers
