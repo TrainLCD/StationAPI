13ab8a63a5db1d3c3b349efef8608c070a851dd9
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-04-20 22:33:08 +0900

    不要なclone()を削除

--- stationapi/src/use_case/interactor/query.rs
+++ stationapi/src/use_case/interactor/query.rs
@@ -388,7 +388,7 @@ where
                         .cloned()
                         .map(Box::new)
                     {
-                        station.train_type = Some(tt.clone());
+                        station.train_type = Some(tt);
                     };
                     line.station = Some(station.clone());
                 }
