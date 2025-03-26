650be469ce03088289bc24d18f62dd30ab972fd7
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-03-26 19:53:09 +0900

    正規化処理の拡充

--- stationapi/src/domain/normalize.rs
+++ stationapi/src/domain/normalize.rs
@@ -4,7 +4,14 @@ pub fn normalize_for_search(input: &str) -> String {
         .map(|c| {
             if ('ぁ'..='ん').contains(&c) {
                 // ひらがな → カタカナ
+                // 変換に失敗した場合は元の文字を返す
                 std::char::from_u32(c as u32 + 0x60).unwrap_or(c)
+            } else if ('０'..='９').contains(&c) {
+                // 全角数字 → 半角数字
+                std::char::from_u32(c as u32 - 0xFEE0).unwrap_or(c)
+            } else if ('Ａ'..='Ｚ').contains(&c) || ('ａ'..='ｚ').contains(&c) {
+                // 全角英字 → 半角英字
+                std::char::from_u32(c as u32 - 0xFEE0).unwrap_or(c)
             } else {
                 c
             }
